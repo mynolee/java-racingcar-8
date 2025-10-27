8acc891a86e89bd6316cbe2cd573b0f9ac031970
leeminho <tommymh97@naver.com>
2025-10-27 23:47:16 +0900

    feat : 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -56,4 +56,9 @@ public class Application {
         }
         return scores;
     }
+
+    private static int readTryTimes() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
 }
